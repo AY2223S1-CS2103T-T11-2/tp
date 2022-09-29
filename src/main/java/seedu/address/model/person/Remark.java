60eb1e6427fe960641bff5038bbfbfd5905e969a
chantellyu <e0758927@u.nus.edu>
2022-09-30 02:42:08 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a remark.
+     *
+     * @param remark Remark to be added.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
