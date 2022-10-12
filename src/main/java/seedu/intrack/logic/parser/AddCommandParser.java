b6e4c89b147986bb3142a284d6878d13f97054f5
chantellyu <e0758927@u.nus.edu>
2022-10-12 21:32:53 +0800

    Refactor AddressBook into InTrack
    AddressBook does not accurately represent the current project.
    
    Let's change all mentions of AddressBook to InTrack.

---
+++ src/main/java/seedu/intrack/logic/parser/AddCommandParser.java
@@ -0,0 +1,67 @@
+package seedu.intrack.logic.parser;
+
+import static seedu.intrack.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.intrack.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.intrack.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.intrack.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.intrack.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.intrack.logic.parser.CliSyntax.PREFIX_POSITION;
+import static seedu.intrack.logic.parser.CliSyntax.PREFIX_STATUS;
+import static seedu.intrack.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import seedu.intrack.logic.commands.AddCommand;
+import seedu.intrack.logic.parser.exceptions.ParseException;
+import seedu.intrack.model.internship.Address;
+import seedu.intrack.model.internship.Email;
+import seedu.intrack.model.internship.Internship;
+import seedu.intrack.model.internship.Name;
+import seedu.intrack.model.internship.Phone;
+import seedu.intrack.model.internship.Position;
+import seedu.intrack.model.internship.Status;
+import seedu.intrack.model.tag.Tag;
+
+/**
+ * Parses input arguments and creates a new AddCommand object
+ */
+public class AddCommandParser implements Parser<AddCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * and returns an AddCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_POSITION, PREFIX_PHONE, PREFIX_EMAIL,
+                        PREFIX_STATUS, PREFIX_ADDRESS, PREFIX_TAG);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_POSITION, PREFIX_PHONE, PREFIX_EMAIL,
+                PREFIX_STATUS, PREFIX_ADDRESS) || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+        Position position = ParserUtil.parsePosition(argMultimap.getValue(PREFIX_POSITION).get());
+        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
+        Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
+        Status status = ParserUtil.parseStatus(argMultimap.getValue(PREFIX_STATUS).get());
+        Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
+        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+
+        Internship internship = new Internship(name, position, phone, email, status, address, tagList);
+
+        return new AddCommand(internship);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+}
